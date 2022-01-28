3620efc571303ddbfe76474e4e8c4e11c0a20036
Siarhey Averburh <Averbyrg@gmail.com>
2022-01-28 21:11:04 +0300

    StartAndroid.ru

---
+++ p0041basicviews/src/main/java/ru/startandroid/p0041basicviews/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.startandroid.p0041basicviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
